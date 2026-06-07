b5650056e87143d69952be52bdf6370045819b52
ojribi-accor <oussama.jribi@consulting-for.accor.com>
2026-06-07 16:44:06 +0200

    bump version 1.49

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.milos_achats"
         minSdk = 24
         targetSdk = 35
-        versionCode = 148
-        versionName = "1.48"
+        versionCode = 149
+        versionName = "1.49"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
