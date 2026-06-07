f75f3539f90d2e38171cd696d7e4448b9ba5bb88
ojribi-accor <oussama.jribi@consulting-for.accor.com>
2026-06-07 16:17:22 +0200

    bump version 1.46

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.milos_achats"
         minSdk = 24
         targetSdk = 35
-        versionCode = 145
-        versionName = "1.45"
+        versionCode = 146
+        versionName = "1.46"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
